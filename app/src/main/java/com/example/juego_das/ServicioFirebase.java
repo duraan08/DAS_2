8d12eec17270637b41378e9f88d7bd2f7df7baa4
unai <unaiduran.upv@gmail.com>
2023-04-17 14:18:40 +0200

    - Mensajeria Firebase

---
+++ app/src/main/java/com/example/juego_das/ServicioFirebase.java
@@ -0,0 +1,29 @@
+package com.example.juego_das;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class ServicioFirebase extends FirebaseMessagingService {
+    public ServicioFirebase() {
+    }
+
+    public void onMessageReceived (RemoteMessage remoteMessage){
+        if (remoteMessage.getData().size() > 0){
+
+        }
+        if (remoteMessage.getNotification() != null){
+
+        }
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+    }
+}
